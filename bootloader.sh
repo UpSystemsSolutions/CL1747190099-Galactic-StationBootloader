71e2d6a637a5a613313e6c852f67110a63ec747b
Jean-Félix Laflamme <jeanfelix.laflamme@gmail.com>
2022-03-08 10:08:42 -0500

    Remove tmp folder once done
    Added code to remove the bltmp folder once the update operation is done

--- bootloader.sh
+++ bootloader.sh
@@ -1,7 +1,10 @@
 mkdir /home/pi/bltmp
 mv /home/pi/bootloader/config/* /home/pi/bltmp
 rm -rf /home/pi/bootloader
+
 git clone https://github.com/UpSystemsSolutions/CL1747190099-Galactic-StationBootloader.git /home/pi/bootloarder
+
 mv /home/pi/bltmp/* /home/pi/bootloader/config
+rm -rf /home/pi/bltmp
 
 bash /home/pi/bootloader/bootGalacticStation.sh

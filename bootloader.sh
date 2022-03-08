0937197c17af5d0b19bbcbbc0ba55b1b75bf6472
Jean-Félix Laflamme <jeanfelix.laflamme@gmail.com>
2022-03-08 10:14:30 -0500

    Added print out to console to follow operation in terminal

--- bootloader.sh
+++ bootloader.sh
@@ -1,10 +1,27 @@
+echo Creating temporary workspace
 mkdir /home/pi/bltmp
+echo Operation Completed
+
+echo Moving the config files to the temporary workspace
 mv /home/pi/bootloader/config/* /home/pi/bltmp
+echo Operation Completed
+
+echo Removing the old code
 rm -rf /home/pi/bootloader
+echo Operation Completed
 
+echo downloading updated code
 git clone https://github.com/UpSystemsSolutions/CL1747190099-Galactic-StationBootloader.git /home/pi/bootloarder
+echo Operation Completed
 
+echo Moving the config files from the temporary workspace to the update bootloader workspace
 mv /home/pi/bltmp/* /home/pi/bootloader/config
-rm -rf /home/pi/bltmp
+echo Operation Completed
 
+echo Cleaning up...
+rm -rf /home/pi/bltmp
+echo Operation Completed
+
+echo Run the boot script
 bash /home/pi/bootloader/bootGalacticStation.sh
+echo Operation Completed

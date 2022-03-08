67ff4be035b52e403317891665d0ddb64454ae83
Jean-Félix Laflamme <jeanfelix.laflamme@gmail.com>
2022-03-08 10:44:23 -0500

    added delimiter to bootloader output

--- bootloader.sh
+++ bootloader.sh
@@ -2,28 +2,43 @@ echo Creating temporary workspace
 mkdir /home/pi/bltmp
 echo Operation Completed
 
+echo ------------------------------------------------------------
+
 echo Moving the config files to the temporary workspace
 mv /home/pi/bootloader/config/* /home/pi/bltmp
 echo Operation Completed
+
+echo ------------------------------------------------------------
 
 echo Removing the old code
 rm -rf /home/pi/bootloader
 echo Operation Completed
 
+echo ------------------------------------------------------------
+
 echo downloading updated code
 git clone https://github.com/UpSystemsSolutions/CL1747190099-Galactic-StationBootloader.git /home/pi/bootloader
 echo Operation Completed
+
+echo ------------------------------------------------------------
 
 echo Moving the config files from the temporary workspace to the update bootloader workspace
 mv /home/pi/bltmp/* /home/pi/bootloader/config
 echo Operation Completed
 
+echo ------------------------------------------------------------
+
 echo Cleaning up...
 rm -rf /home/pi/bltmp
 echo Operation Completed
 
+echo ------------------------------------------------------------
+
 echo Updating the autostart code
 mv /home/pi/bootloader/autostart /home/pi/.config/lxsession/LXDE-pi/autostart
+echo Operation Completed
+
+echo ------------------------------------------------------------
 
 echo Run the boot script
 bash /home/pi/bootloader/bootGalacticStation.sh

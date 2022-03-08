4bcdbf063f41bfe21bab0eea871e73b9b9e080f9
Jean-Félix Laflamme <jeanfelix.laflamme@gmail.com>
2022-03-08 10:01:01 -0500

    Fix typo

--- bootloader.sh
+++ bootloader.sh
@@ -1,7 +1,7 @@
 mkdir /home/pi/bltmp
-mv /home/pi/bootloarder/config/* /home/pi/bltmp
-rm -rf /home/pi/bootloarder
+mv /home/pi/bootloader/config/* /home/pi/bltmp
+rm -rf /home/pi/bootloader
 git clone https://github.com/UpSystemsSolutions/CL1747190099-Galactic-StationBootloader.git /home/pi/bootloarder
-mv /home/pi/bltmp/* /home/pi/bootloarder/config
+mv /home/pi/bltmp/* /home/pi/bootloader/config
 
-bash /home/pi/bootloarder/bootGalacticStation.sh
+bash /home/pi/bootloader/bootGalacticStation.sh

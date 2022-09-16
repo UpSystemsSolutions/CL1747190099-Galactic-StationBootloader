a7eaa7c329339727f3231ee568b36f94d5c489af
Jean-Félix Laflamme <jeanfelix.laflamme@gmail.com>
2022-09-16 10:44:31 -0400

    Update bootGalacticStation.sh

--- bootGalacticStation.sh
+++ bootGalacticStation.sh
@@ -2,5 +2,5 @@
 StationDashboardURL="$CoreProtocol://$CoreAddress:$CorePort/#/operator/$StationID"
 
 chromium-browser --start-fullscreen --start-maximized --kiosk --incognito $StationDashboardURL 
-python3 /home/pi/rpi-power-monitor/power-monitor.py terminal
+python3 /home/pi/rpi-power-monitor/power-monitor.py
 #echo $StationDashboardURL

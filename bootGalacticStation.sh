14cec6a9bd4e6cade8079800ee56aa96dceb9b50
Jean-Félix Laflamme <jeanfelix.laflamme@gmail.com>
2022-09-16 10:10:34 -0400

    update to bootlGalacticStations
    Updated to chromium kiosk mode + added py3 to launch power-monitor at boot.

--- bootGalacticStation.sh
+++ bootGalacticStation.sh
@@ -1,5 +1,6 @@
 . /home/pi/bootloader/config/station.config
 StationDashboardURL="$CoreProtocol://$CoreAddress:$CorePort/#/operator/$StationID"
 
-chromium-browser --incognito $StationDashboardURL
+chromium-browser --start-fullscreen --start-maximized --kiosk --incognito $StationDashboardURL 
+python3 /home/pi/rpi-power-monitor/power-monitor.py terminal
 #echo $StationDashboardURL

6422b65064e0514f30e2ff402cc3e61533c192d6
Jean-Félix Laflamme <jeanfelix.laflamme@gmail.com>
2022-09-16 09:48:15 -0400

    Switch to Chromium

--- bootGalacticStation.sh
+++ bootGalacticStation.sh
@@ -1,5 +1,5 @@
 . /home/pi/bootloader/config/station.config
 StationDashboardURL="$CoreProtocol://$CoreAddress:$CorePort/#/operator/$StationID"
 
-firefox $StationDashboardURL
+chromium-browser --incognito $StationDashboardURL
 #echo $StationDashboardURL
